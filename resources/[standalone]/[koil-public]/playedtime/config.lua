9251714db33fae248f2fe3197962f1286d5db7e9
SkyHavenRP <skyhavenrp@gmail.com>
2022-04-14 10:20:42 +0100

    koil

---
+++ resources/[standalone]/[koil-public]/playedtime/config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+Config.Webhook = false
+Config.WebhookLink = 'https://discord.com/api/webhooks/963468401756471356/7o4eJ2GVdH6RXarH_CATCm-h3qgMlqIHwWXMID8e7HfrXU_GodyRX_Vnt3qKbnNX3W5J'
+
+Config.Strings= {
+	['playerleft'] = 'NADRP SERVER: Player left',
+	['sessiontime'] = 'Session time',
+	['totaltime'] = 'Total time',
+	-- player notifications
+	['welcome1st'] = 'Welcome to NADRP!',
+	['welcome'] = 'Welcome back!',
+	['ptotaltime'] = 'Total time spent on server: ',
+	['loggedin'] = 'You have logged in ~y~%s~s~ times',
+}
