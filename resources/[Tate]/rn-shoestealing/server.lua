ee203d1bd57e91e59a0253a11bfa2cdb24c191b2
SkyHavenRP <skyhavenrp@gmail.com>
2022-04-28 12:11:01 +0300

    Files moved p3 end

--- resources/[Tate]/rn-shoestealing/server.lua
+++
@@ -1,9 +0,0 @@
-local DenaliFW = exports['denalifw-core']:GetCoreObject()
-
-
-RegisterServerEvent('rn-shoestealing:serverShoesSteal')
-AddEventHandler('rn-shoestealing:serverShoesSteal', function(TargetSource)
-    local Player = DenaliFW.Functions.GetPlayer(source)
-    Player.Functions.AddItem("weapon_shoe", 1, false, false, true)
-    TriggerClientEvent('rn-shoestealing:ShoesRemove', TargetSource)
-end)
